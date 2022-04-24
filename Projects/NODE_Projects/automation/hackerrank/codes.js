5534d444953c593b4590b4ac2100e3b7c1864244
karunasagar555 <skarunasagar3@gmail.com>
2022-04-24 21:41:52 +0530

    HackerRank Browser Automation Completed

--- Projects/NODE_Projects/automation/hackerrank/codes.js
+++ Projects/NODE_Projects/automation/hackerrank/codes.js
@@ -1,107 +1,128 @@
 module.exports = {
-  answer: [
-    `#include <bits/stdc++.h>
-          using namespace std;
-          
-          int main(){
-              int number_of_elements;
-              cin >> number_of_elements;
-              vector <int> array(number_of_elements);
-              int sum_of_array = 0;
-              
-              for(int i = 0; i < number_of_elements; i++){
-                 cin >> array[i];
-                 sum_of_array += array[i];
-              }
-              
-              cout << sum_of_array;
-              return 0;
-          }
-  `,
-    `#include <bits/stdc++.h>
-  using namespace std;
-  string ltrim(const string &);
-  string rtrim(const string &);
-  vector<string> split(const string &);
-  /*
-   * Complete the 'compareTriplets' function below.
-   *
-   * The function is expected to return an INTEGER_ARRAY.
-   * The function accepts following parameters:
-   *  1. INTEGER_ARRAY a
-   *  2. INTEGER_ARRAY b
-   */
-  vector<int> compareTriplets(vector<int> a, vector<int> b) {
-      int alice = 0;
-      int bob = 0;
-      vector <int> answer(2);
-      for(int i = 0; i < 3; i++) {
-          if (a[i] > b[i]) alice++;
-          if (a[i] < b[i]) bob++;
-      }
-      answer[0] = alice;
-      answer[1] = bob;
-      return answer;
-  }
-  int main()
-  {
-      ofstream fout(getenv("OUTPUT_PATH"));
-      string a_temp_temp;
-      getline(cin, a_temp_temp);
-      vector<string> a_temp = split(rtrim(a_temp_temp));
-      vector<int> a(3);
-      for (int i = 0; i < 3; i++) {
-          int a_item = stoi(a_temp[i]);
-          a[i] = a_item;
-      }
-      string b_temp_temp;
-      getline(cin, b_temp_temp);
-      vector<string> b_temp = split(rtrim(b_temp_temp));
-      vector<int> b(3);
-      for (int i = 0; i < 3; i++) {
-          int b_item = stoi(b_temp[i]);
-          b[i] = b_item;
-      }
-      vector<int> result = compareTriplets(a, b);
-      for (size_t i = 0; i < result.size(); i++) {
-          fout << result[i];
-          if (i != result.size() - 1) {
-              fout << " ";
-          }
-      }
-      fout << "\n";
-      fout.close();
-      return 0;
-  }
-  string ltrim(const string &str) {
-      string s(str);
-      s.erase(
-          s.begin(),
-          find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
-      );
-      return s;
-  }
-  string rtrim(const string &str) {
-      string s(str);
-      s.erase(
-          find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
-          s.end()
-      );
-      return s;
-  }
-  vector<string> split(const string &str) {
-      vector<string> tokens;
-      string::size_type start = 0;
-      string::size_type end = 0;
-      while ((end = str.find(" ", start)) != string::npos) {
-          tokens.push_back(str.substr(start, end - start));
-          start = end + 1;
-      }
-      tokens.push_back(str.substr(start));
-      return tokens;
-  }
-  `,
-    `#include <bits/stdc++.h>
+  "Simple Array Sum": `
+        // simple array sum
+        #include <bits/stdc++.h>
+       using namespace std;
+        
+       int main(){
+           int number_of_elements;
+           cin >> number_of_elements;
+           vector <int> array(number_of_elements);
+           int sum_of_array = 0;
+           
+           for(int i = 0; i < number_of_elements; i++){
+              cin >> array[i];
+              sum_of_array += array[i];
+           }
+           
+           cout << sum_of_array;
+           return 0;
+       }
+    `,
+  "Solve Me First": `//solve me first
+        #include <iostream>
+        using namespace std;
+        
+        int solveMeFirst(int a, int b) {
+            return a + b;
+        }
+        
+        int main() {
+          int num1, num2;
+          int sum;
+          cin>>num1>>num2;
+          sum = solveMeFirst(num1,num2);
+          cout<<sum;
+          return 0;
+        }`,
+  "Compare the Triplets": `
+  // Compare the triplets
+         #include <bits/stdc++.h>
+        using namespace std;
+        string ltrim(const string &);
+        string rtrim(const string &);
+        vector<string> split(const string &);
+        /*
+         * Complete the 'compareTriplets' function below.
+         *
+         * The function is expected to return an INTEGER_ARRAY.
+         * The function accepts following parameters:
+         *  1. INTEGER_ARRAY a
+         *  2. INTEGER_ARRAY b
+         */
+        vector<int> compareTriplets(vector<int> a, vector<int> b) {
+            int alice = 0;
+            int bob = 0;
+            vector <int> answer(2);
+            for(int i = 0; i < 3; i++) {
+                if (a[i] > b[i]) alice++;
+                if (a[i] < b[i]) bob++;
+            }
+            answer[0] = alice;
+            answer[1] = bob;
+            return answer;
+        }
+        int main()
+        {
+            ofstream fout(getenv("OUTPUT_PATH"));
+            string a_temp_temp;
+            getline(cin, a_temp_temp);
+            vector<string> a_temp = split(rtrim(a_temp_temp));
+            vector<int> a(3);
+            for (int i = 0; i < 3; i++) {
+                int a_item = stoi(a_temp[i]);
+                a[i] = a_item;
+            }
+            string b_temp_temp;
+            getline(cin, b_temp_temp);
+            vector<string> b_temp = split(rtrim(b_temp_temp));
+            vector<int> b(3);
+            for (int i = 0; i < 3; i++) {
+                int b_item = stoi(b_temp[i]);
+                b[i] = b_item;
+            }
+            vector<int> result = compareTriplets(a, b);
+            for (size_t i = 0; i < result.size(); i++) {
+                fout << result[i];
+                if (i != result.size() - 1) {
+                    fout << " ";
+                }
+            }
+            fout << "\\n";
+            fout.close();
+            return 0;
+        }
+        string ltrim(const string &str) {
+            string s(str);
+            s.erase(
+                s.begin(),
+                find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+            );
+            return s;
+        }
+        string rtrim(const string &str) {
+            string s(str);
+            s.erase(
+                find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+                s.end()
+            );
+            return s;
+        }
+        vector<string> split(const string &str) {
+            vector<string> tokens;
+            string::size_type start = 0;
+            string::size_type end = 0;
+            while ((end = str.find(" ", start)) != string::npos) {
+                tokens.push_back(str.substr(start, end - start));
+                start = end + 1;
+            }
+            tokens.push_back(str.substr(start));
+            return tokens;
+        }
+    `,
+  "A Very Big Sum": `
+  //a very big sum
+  #include <bits/stdc++.h>
   using namespace std;
   string ltrim(const string &);
   string rtrim(const string &);
@@ -132,7 +153,7 @@ module.exports = {
           ar[i] = ar_item;
       }
       long result = aVeryBigSum(ar);
-      fout << result << "\n";
+      fout << result << "\\n";
       fout.close();
       return 0;
   }
@@ -162,16 +183,20 @@ module.exports = {
       }
       tokens.push_back(str.substr(start));
       return tokens;
-  }
-  `,
-    `#include <iostream>
+  }`,
+  "Diagonal Difference": `
+  #include <iostream>
   using namespace std;
+  
   int main() {
       int n;
       cin >> n;
+  
       int arr[n][n];
+  
       long long int d1=0; //First Diagonal
       long long int d2=0; //Second Diagonal
+  
       for (int i = 0; i < n; i++) {
           for (int j = 0; j < n; j++) {
               cin >> arr[i][j];
@@ -179,8 +204,8 @@ module.exports = {
               if (i == n - j - 1) d2 += arr[i][j];
           }
       }
+  
       cout << abs(d1 - d2) << endl; //Absolute difference of the sums across the diagonals
       return 0;
   }`,
-  ],
 };
--- Projects/NODE_Projects/automation/hackerrank/hackerrank.js
+++ Projects/NODE_Projects/automation/hackerrank/hackerrank.js
@@ -1,208 +1,232 @@
 const puppeteer = require("puppeteer");
 
-let {email, password } = require('./secrets');
+let { email, password } = require("./secrets");
 
 // let email = "";
 // let password = "";
 
-let { answer } = require("./codes");
+let codes = require("./codes");
 
 // console.log("Email", email);
 
 let currTab;
-let browserOpenPromise = puppeteer.launch                           //how
-({                                                                  //should
-    headless : false,                                               //be
-    defaultViewport:null,                                           //our
-    args: ["--start-maximized"]                                     //browser
-});
+let browserOpenPromise = puppeteer.launch(
+  //how
+  {
+    //should
+    headless: false, //be
+    defaultViewport: null, //our
+    args: ["--start-maximized"], //browser
+  }
+);
 
 //console.log(browserOpenPromise);
 
 browserOpenPromise //fulfill
-    .then(function (browser){     // got browser (.then is a method which takes function)
+  .then(function (browser) {
+    // got browser (.then is a method which takes function)
     console.log("browser is open");
     //console.log(browserOpenPromise);
     //console.log(browser);
 
     //An array of all open pages inside the Browser.
-    let allTabsPromise = browser.pages();  //pages() method will take and come all the tabs 
+    let allTabsPromise = browser.pages(); //pages() method will take and come all the tabs
     return allTabsPromise; // will return all the tabs                                                   // CHAINING
-    })                                                                                                      // OF
-    .then(function (allTabsArr) {                                                                        //PROMISES
-        currTab = allTabsArr[0]; // currTab->(current tab)
-        console.log("new Tab");
+  }) // OF
+  .then(function (allTabsArr) {
+    //PROMISES
+    currTab = allTabsArr[0]; // currTab->(current tab)
+    console.log("new Tab");
 
-        //url to navigate page to 
-        let visitingLoginPromisePage = currTab.goto("https://www.hackerrank.com/auth/login");
-        return visitingLoginPromisePage;
-    })
-    .then(function (data) {
-        //console.log(data);
-        console.log("hackerRank login page opened");
+    //url to navigate page to
+    let visitingLoginPromisePage = currTab.goto(
+      "https://www.hackerrank.com/auth/login"
+    );
+    return visitingLoginPromisePage;
+  })
+  .then(function (data) {
+    //console.log(data);
+    console.log("hackerRank login page opened");
 
-                                                  //selector(where to type),  data(what to type)
-        let emailWillBeTypedPromise = currTab.type("input[name = 'username']", email,
-        {
-            //delay : 100
+    //selector(where to type),  data(what to type)
+    let emailWillBeTypedPromise = currTab.type(
+      "input[name = 'username']",
+      email,
+      {
+        //delay : 100
+      }
+    );
+    return emailWillBeTypedPromise;
+  })
+  .then(function () {
+    console.log("email is typed");
+    let passwordWillBeTypedPromise = currTab.type(
+      "input[type = 'password']",
+      password,
+      {
+        //delay : 100
+      }
+    );
+    return passwordWillBeTypedPromise;
+  })
+  .then(function () {
+    console.log("password has been typed");
+    let willBeLoggedInPromise = currTab.click(
+      ".ui-btn.ui-btn-large.ui-btn-primary.auth-button.ui-btn-styled"
+    );
+    return willBeLoggedInPromise;
+  })
+  .then(function () {
+    console.log("logged into hackerrank successfully");
+
+    //waitAndClick will wait for the selector to load and then click on the node
+    let algorithmTabWillBeOpenedPromise = waitAndClick(
+      "div[data-automation= 'algorithms']"
+    );
+    return algorithmTabWillBeOpenedPromise;
+  })
+  .then(function () {
+    console.log("algorithms page is opened");
+    let allQuesPromise = currTab.waitForSelector(
+      "a[data-analytics = 'ChallengeListChallengeName']"
+    );
+    return allQuesPromise;
+  })
+  .then(function () {
+    function getAllQuesLinks() {
+      let qEle = document.querySelectorAll("a[data-analytics='ChallengeListChallengeName'] .challengecard-title");
+
+      let allElemArr = document.querySelectorAll(
+        "a[data-analytics = 'ChallengeListChallengeName']"
+      );
+      let linksArr = [];
+      for (let i = 0; i < allElemArr.length; i++) {
+        let name = qEle[i].innerText.split("\n")[0];
+        linksArr.push({
+          question: name,
+          link: allElemArr[i].getAttribute("href"),
         });
-        return emailWillBeTypedPromise;
-    })
-    .then(function () {
-        console.log("email is typed");
-        let passwordWillBeTypedPromise = currTab.type("input[type = 'password']", password, 
-        {
-            //delay : 100
-        });
-        return passwordWillBeTypedPromise
-    })
-    .then(function () {
-        console.log("password has been typed");
-        let willBeLoggedInPromise = currTab.click(".ui-btn.ui-btn-large.ui-btn-primary.auth-button.ui-btn-styled");
-        return willBeLoggedInPromise;
-    })
-    .then(function () {
-        console.log("logged into hackerrank successfully");
-        
-        //waitAndClick will wait for the selector to load and then click on the node
-        let algorithmTabWillBeOpenedPromise = waitAndClick("div[data-automation= 'algorithms']")
-        return algorithmTabWillBeOpenedPromise;
-    })
-    .then(function () {
-        console.log("algorithms page is opened");
-        let allQuesPromise = currTab.waitForSelector("a[data-analytics = 'ChallengeListChallengeName']");
-        return allQuesPromise;
-    })
-    .then(function(){
-        function getAllQuesLinks(){
-            let allElemArr = document.querySelectorAll("a[data-analytics = 'ChallengeListChallengeName']");
-            let linksArr = [];
-            for(let i = 0; i<allElemArr.length; i++){
-                linksArr.push(allElemArr[i].getAttribute("href"));
-            }
-            return linksArr;
-        }
-
-        let linksArrPromise = currTab.evaluate(getAllQuesLinks);
-        return linksArrPromise;
-    })
-    .then(function(linksArr){
-        console.log("links to all ques recieved");
-        console.log("linksArr printed", linksArr);
-        
-                                                         //link to the question to be solved, index of the linksArr
-        let questionWillBeSolvedPromise = questionSolver(linksArr[0], 0) ; // qsns solve karta hai
-        for(let i = 1; i<linksArr.length; i++){
-            questionWillBeSolvedPromise = questionWillBeSolvedPromise.then(function (){
-                return questionSolver(linksArr[i], i);
-            })
-            // a = 10;
-            // a = a + 1;
-        }
-        return questionWillBeSolvedPromise;
-    })
-    .then(function (){
-        console.log("question is solved");
-    })
-    .catch(function (err) {
-        console.log(err);
-    });
-
-
-    function waitAndClick(algoBtn){
-        let waitClickPromise = new Promise(function(resolve, reject){
-            let waitForSelectorPromise = currTab.waitForSelector(algoBtn);
-            waitForSelectorPromise               // if fulfilled
-            .then(function(){
-                console.log("algo btn is found");
-                let clickPromise = currTab.click(algoBtn);
-                return clickPromise;
-            })
-            .then(function(){
-                console.log("algo btn is clicked");
-                resolve();
-            })
-            .catch(function(err){
-                reject(err);
-            })
-        });
-        return waitClickPromise;
+      }
+      return linksArr;
     }
-    
 
-    function questionSolver(url,idx){
-        return new Promise(function(resolve,reject){
-                let fullLink = `https://www.hackerrank.com${url}`;
-                let goToQuesPagePromise = currTab.goto(fullLink);
-                goToQuesPagePromise
-                .then(function(){
-                console.log("question opened");
-                // check the custom input box mark
-                let waitForCheckBoxAndClickPromise = waitAndClick(".checkbox-input");
-                return waitForCheckBoxAndClickPromise;
-            })
-            .then(function (){
-                // Select the box where code will be typed
-                let waitForTextboxToAppearPromise = currTab.waitForSelector(".custominput");
-                return waitForTextboxToAppearPromise;
-            })
-            .then(function (){
-                let codeWillBeTypedPromise = currTab.type(".custominput", answer[idx],
-                {
-                    //delay : 200
-                });
-                return codeWillBeTypedPromise;
-            })
-            .then(function (){
-                //control key is pressed promise
-                let controlPressedPromise = currTab.keyboard.down("Control");
-                return controlPressedPromise;
-            })
-            .then(function (){
-                let aKeyPressedPromise = currTab.keyboard.press("a");
-                return aKeyPressedPromise;
-            })
-            .then(function (){
-                let xKeyPressedPromise = currTab.keyboard.press("x");
-                return xKeyPressedPromise;
-            })
-            .then(function (){
-                let ctrlIsReleasedPromise = currTab.keyboard.up("Control");
-                return ctrlIsReleasedPromise;
-            })
-            .then(function (){
-                //select the editor promise
-                let cursorOnEditorPromise = currTab.click(".monaco-editor.no-user-select.vs");
-                return cursorOnEditorPromise;
-            })
-            .then(function (){
-                // control key is pressed promise
-                let controlPressedPromise = currTab.keyboard.down("Control");
-                return controlPressedPromise;
-            })
-            .then(function (){
-                let aKeyPressedPromise = currTab.keyboard.press("A");
-                return aKeyPressedPromise;
-            })
-            .then(function (){
-                let vKeyPressedPromise = currTab.keyboard.press("V");
-                return vKeyPressedPromise;
-            })
-            .then(function (){
-                let controlDownPromise = currTab.keyboard.up("Control");
-                return controlDownPromise;
-            })
-            .then(function (){
-                let submitButtonClickedPromise = currTab.click(".hr-monaco-submit");
-                return submitButtonClickedPromise;
-            })
-            .then(function (){
-                console.log("code submitted successfully");
-                resolve();
-            })
-            .catch(function(err){
-                reject(err);
-            });
-        });
-        
+    let linksArrPromise = currTab.evaluate(getAllQuesLinks);
+    return linksArrPromise;
+  })
+  .then(async function (linksArr) {
+    console.log("links to all ques recieved");
+    console.log("linksArr printed", linksArr);
+
+    //link to the question to be solved, index of the linksArr
+    let questionWillBeSolvedPromise;
+    for (let i = 0; i < linksArr.length; i++) {
+        if(codes[linksArr[i].question]) {
+            questionWillBeSolvedPromise = await questionSolver(linksArr[i]); // qsns solve karta hai
+        } 
     }
+    return questionWillBeSolvedPromise;
+    //return new Promise.resolve("done");
+  })
+  .then(function () {
+    console.log("question is solved");
+  })
+  .catch(function (err) {
+    console.log(err);
+  });
+
+function waitAndClick(algoBtn) {
+  let waitClickPromise = new Promise(function (resolve, reject) {
+    let waitForSelectorPromise = currTab.waitForSelector(algoBtn);
+    waitForSelectorPromise // if fulfilled
+      .then(function () {
+        let clickPromise = currTab.click(algoBtn);
+        return clickPromise;
+      })
+      .then(function () {
+        resolve();
+      })
+      .catch(function (err) {
+        reject(err);
+      });
+  });
+  return waitClickPromise;
+}
+
+function questionSolver(q) {
+  return new Promise(function (resolve, reject) {
+    let fullLink = `https://www.hackerrank.com${q.link}`;
+    let goToQuesPagePromise = currTab.goto(fullLink);
+    goToQuesPagePromise
+      .then(function () {
+        
+        // check the custom input box mark
+        let waitForCheckBoxAndClickPromise = waitAndClick(".checkbox-input");
+        return waitForCheckBoxAndClickPromise;
+      })
+      .then(function () {
+        // Select the box where code will be typed
+        let waitForTextboxToAppearPromise =
+          currTab.waitForSelector(".custominput");
+        return waitForTextboxToAppearPromise;
+      })
+      .then(function () {
+          let codeWillBeTypedPromise = currTab.type(".custominput",codes[q.question]);
+          return codeWillBeTypedPromise;
+      })
+      .then(function () {
+        //control key is pressed promise
+        let controlPressedPromise = currTab.keyboard.down("Control");
+        return controlPressedPromise;
+      })
+      .then(function () {
+        let aKeyPressedPromise = currTab.keyboard.press("a");
+        return aKeyPressedPromise;
+      })
+      .then(function () {
+        let xKeyPressedPromise = currTab.keyboard.press("x");
+        return xKeyPressedPromise;
+      })
+      .then(function () {
+        let ctrlIsReleasedPromise = currTab.keyboard.up("Control");
+        return ctrlIsReleasedPromise;
+      })
+      .then(function () {
+        //select the editor promise
+        let cursorOnEditorPromise = currTab.click(
+          ".monaco-editor.no-user-select.vs"
+        );
+        return cursorOnEditorPromise;
+      })
+      .then(function () {
+        // control key is pressed promise
+        let controlPressedPromise = currTab.keyboard.down("Control");
+        return controlPressedPromise;
+      })
+      .then(function () {
+        let aKeyPressedPromise = currTab.keyboard.press("a");
+        return aKeyPressedPromise;
+      })
+      .then(function () {
+        let vKeyPressedPromise = currTab.keyboard.press("v");
+        return vKeyPressedPromise;
+      })
+      .then(function () {
+        let controlDownPromise = currTab.keyboard.up("Control");
+        return controlDownPromise;
+      })
+      .then(function () {
+        let submitButtonClickedPromise = currTab.click(".hr-monaco-submit");
+        return submitButtonClickedPromise;
+      })
+      .then(function() {
+          return currTab.waitFor(5000);
+      })
+      .then(function () {
+        console.log(q.question,"code submitted successfully");
+        resolve();
+      })
+      .catch(function (err) {
+        reject(err);
+      });
+  });
+}
